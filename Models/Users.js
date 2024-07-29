ad465aeb2ee2d02120395bfa30ec7c15c410ca8a
Mariepo <marie.potte.62@gmail.com>
2024-07-29 12:07:54 +0200

    ajout du model Users

---
+++ Models/Users.js
@@ -0,0 +1,67 @@
+const { Model, DataTypes} = require("sequelize");
+const sequelize = require("../Config/Sequelize");
+
+class Users extends Model {
+
+}
+
+Users.init({
+    id : {
+        type : DataTypes.INTEGER,
+        primaryKey : true,
+        autoIncrement : true
+    },
+    name : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    firstname : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    password : {
+        type : DataTypes.TEXT,
+        allowNull : false
+    },
+    email: {
+        type : DataTypes.STRING,
+        allowNull : false,
+        unique : true
+    },
+    phone : {
+        type : DataTypes.STRING,
+        allowNull : false,
+        unique : true
+    },
+    address : {
+        type : DataTypes.STRING,
+        allowNull : true,
+    },
+    postal_code : {
+        type : DataTypes.STRING,
+        allowNull : true,
+    },
+    city : {
+        type : DataTypes.STRING,
+        allowNull : true,
+    },
+    is_admin : {
+        type : DataTypes.TINYINT,
+        allowNull : true,
+    },
+        created_at : {
+        type : DataTypes.DATE,
+        allowNull : true
+    },
+    updated_at : {
+        type : DataTypes.DATE,
+        allowNull : true
+    }
+}, {
+        sequelize,
+        modelName : "Users",
+        tableName : "users",
+        timestamps : false
+    })
+
+module.exports = Users;
