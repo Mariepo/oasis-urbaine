484c287ad7ac64e98a61932a112b6b02ce232cc8
Mariepo <marie.potte.62@gmail.com>
2024-07-31 10:42:44 +0200

    responsive des cards sur la homepage

--- oasis-urbaine-front/src/Pages/Homepage.jsx
+++ oasis-urbaine-front/src/Pages/Homepage.jsx
@@ -41,8 +41,7 @@ function HomePage() {
 
     <Row className="g-4">
         {featuredProducts.map((product) => (
-            <Col key={product.id} className='col-xs-12 col-6 col-lg-3' >
-                {/* xs={12} sm={6} md={6} lg={3}   */}
+            <Col xs={12} sm={6} md={6} lg={3} key={product.id} >
                 <ProductCard product={product}/>
             </Col>
         ))}
@@ -70,14 +69,14 @@ function HomePage() {
         <Row className='d-flex justify-content-between mb-5'>
             <h2 className='mt-5 text-center'>Des plantes en bonne santé</h2>
         </Row>
-        <Row>
-            <Col className='col-xs-12 col-4'>
+        <Row className="g-4">
+            <Col xs={12} sm={6} md={4} lg={4}>
                 <ValueCard videoSource="assets/videos/value-natural.mp4" title="Plantes Éco-Responsables" description="Nous offrons des arbres cultivés sans pesticides ni produits chimiques nocifs, pour  des arbres naturellement en bonne santé et le respect de l’environnement."></ValueCard>
             </Col>
-            <Col className='col-xs-12 col-4'>
+            <Col xs={12} sm={6} md={4} lg={4}>
                 <ValueCard videoSource="assets/videos/value-delivery.mp4" title="Livraison aux petits soins" description="Notre emballage triple cannelure renforcée assure que vos plantes arrivent en  parfait état. Le feuillage est protégé et l'arbre reste bien droit."></ValueCard>
             </Col> 
-            <Col className='col-xs-12 col-4'>
+            <Col xs={12} sm={6} md={4} lg={4}>
                 <ValueCard videoSource="assets/videos/value-healthy-plant.mp4" title="Des variétés résistantes" description="Nous choisissons rigoureusement nos variétés pour assurer que chaque plante soit durable. Nous assurons des apports en minéraux et vitamines pour les plantes aient un système racinaire solide."></ValueCard>
             </Col>
         </Row>
