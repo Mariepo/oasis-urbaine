e8c63907bc73989c5ece5025019738aa66eccfe9
Mariepo <marie.potte.62@gmail.com>
2024-08-01 14:51:41 +0200

    ajout de la génération et vérification des tokens JWT

--- oasis-urbaine-api/Routes/ProductsRoutes.js
+++ oasis-urbaine-api/Routes/ProductsRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const ProductsController = require('../Controllers/ProductsController');
+const AuthenticateController = require('../Controllers/AuthenticateController');
 
 const router = express.Router();
 
 
 router.get("/", (request, result) => {ProductsController.getAllProducts(request, result)});
 router.get("/:id", (request, result) => {ProductsController.getProductById(request, result)});
-router.post("/", (request, result) => {ProductsController.addProduct(request, result)});
-router.patch("/:id", (request, result) => {ProductsController.updateProduct(request, result)});
-router.delete("/:id", (request, result) => {ProductsController.deleteProduct(request, result)});
+router.post("/", AuthenticateController.authenticateToken, (request, result) => {ProductsController.addProduct(request, result)});
+router.patch("/:id", AuthenticateController.authenticateToken, (request, result) => {ProductsController.updateProduct(request, result)});
+router.delete("/:id", AuthenticateController.authenticateToken, (request, result) => {ProductsController.deleteProduct(request, result)});
 
 module.exports = router;
