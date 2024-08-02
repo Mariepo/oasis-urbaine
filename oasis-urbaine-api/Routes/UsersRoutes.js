d77b2a9281d65e2e6bc601df9f76506aacd6b18d
Mariepo <marie.potte.62@gmail.com>
2024-08-02 18:14:45 +0200

    ajout de la récupération des informations de l'utilisateur connecté

--- oasis-urbaine-api/Routes/UsersRoutes.js
+++ oasis-urbaine-api/Routes/UsersRoutes.js
@@ -5,7 +5,7 @@ const AuthenticateController = require('../Controllers/AuthenticateController');
 const router = express.Router();
 
 router.get("/", (request, result) => {UsersController.getAllUser(request, result)});
-router.get("/:id", (request, result) => {UsersController.getUserById(request, result)});
+router.get("/:id", AuthenticateController.authenticateToken, (request, result) => {UsersController.getUserById(request, result)});
 router.post("/signup", (request, result) => {UsersController.addUser(request, result)});
 router.post("/login", (request, result) => {UsersController.loginUser(request, result)});
 router.patch("/:id", AuthenticateController.authenticateToken, (request, result) => {UsersController.updateUser(request, result)});
