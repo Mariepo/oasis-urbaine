14d98e228e2f9ad5c968b810cd8636bcd8e259a3
Mariepo <marie.potte.62@gmail.com>
2024-08-06 17:58:09 +0200

    changement de la route /orders/id/user par /orders/user/id

--- oasis-urbaine-front/src/Services/OrdersService.js
+++ oasis-urbaine-front/src/Services/OrdersService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class OrdersService {
     static fetchOrdersById(id) {
-        return axios.get(`http://127.0.0.1:3001/orders/${id}/user`);
+        return axios.get(`http://127.0.0.1:3001/orders/user`+id);
     }
 }
 
