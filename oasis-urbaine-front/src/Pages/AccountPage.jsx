27d6b9fb3f409aca94290f97cbf86a044961627f
Mariepo <marie.potte.62@gmail.com>
2024-08-06 17:48:15 +0200

    ajout des commandes sur /account

--- oasis-urbaine-front/src/Pages/AccountPage.jsx
+++ oasis-urbaine-front/src/Pages/AccountPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import AuthContext from '../Context/AuthContext';
 import UsersService from '../Services/UsersService';
+import OrdersService from '../Services/OrdersService';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { Container, Row } from 'react-bootstrap';
@@ -9,6 +10,7 @@ function AccountPage() {
     const navigate = useNavigate();
     const {setIsAuthenticated, setToken} = useContext(AuthContext);
     const [user, setUser] = useState([]);
+    const [orders, setOrders] = useState([]);
     const id = UsersService.getUserId();
 
     // Deconnexion
@@ -30,8 +32,18 @@ function AccountPage() {
         }
     }
 
+    const fetchOrder = async () => {
+        try {
+            const response = await OrdersService.fetchOrdersById(id);
+            setOrders(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     useEffect(() => {
         fetchUser();
+        fetchOrder();
     }, []);
 
     return <>
@@ -45,6 +57,21 @@ function AccountPage() {
         </Row>
         <button onClick={logout}>Deconnexion</button>
     </div>
+    <h2>Historique de commande</h2>
+            {orders.length === 0 ? (
+                <p>Aucune commande disponible.</p>
+            ) : (
+                orders.map((order) => (
+                    <div key={order.id}>
+                        <p>Commande ID: {order.id}</p>
+                        <p>Montant total: {order.total_amount}</p>
+                        <p>Statut: {order.status}</p>
+                        <p>Date de création: {new Date(order.created_at).toLocaleDateString()}</p>
+                        <hr />
+                    </div>
+                ))
+            )}
+    
         </Container>
     </>
 
--- oasis-urbaine-front/src/Services/OrdersService.js
+++ oasis-urbaine-front/src/Services/OrdersService.js
@@ -1,9 +1,9 @@
-// import axios from "axios";
+import axios from "axios";
 
-// class OrdersService {
-//     static fetchOrderById() {
-//         // return axios
-//     }
-// }
+class OrdersService {
+    static fetchOrdersById(id) {
+        return axios.get(`http://127.0.0.1:3001/orders/${id}/user`);
+    }
+}
 
-// export default OrdersService
+export default OrdersService
