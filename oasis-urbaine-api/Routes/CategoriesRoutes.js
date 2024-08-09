f104066102d9c494130bad9fecb82adbdf465685
Mariepo <marie.potte.62@gmail.com>
2024-08-09 17:23:57 +0200

    ajout des composants boostrap sur /signup

--- oasis-urbaine-api/Routes/CategoriesRoutes.js
+++ oasis-urbaine-api/Routes/CategoriesRoutes.js
@@ -5,7 +5,7 @@ const AuthenticateController = require('../Controllers/AuthenticateController');
 const router = express.Router();
 
 
-router.get("/", AuthenticateController.authenticateToken, (request, result) => {CategoriesController.getAllCategories(request, result)});
+router.get("/", (request, result) => {CategoriesController.getAllCategories(request, result)});
 router.get("/:id", (request, result) => {CategoriesController.getCategoryById(request, result)});
 router.post("/", AuthenticateController.authenticateToken, (request, result) => {CategoriesController.addCategory(request, result)});
 router.patch("/:id", AuthenticateController.authenticateToken, (request, result) => {CategoriesController.updateCategory(request, result)});
