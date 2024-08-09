bc57843ea3d23cc1456aa473a44928aa3e769b52
Mariepo <marie.potte.62@gmail.com>
2024-08-09 12:13:34 +0200

    ajout des composants bootstrap sur la page /login

--- oasis-urbaine-front/src/Pages/LoginPage.jsx
+++ oasis-urbaine-front/src/Pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import UsersService from '../Services/UsersService';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import AuthContext from '../Context/AuthContext';
+import {Container, Button, Form} from 'react-bootstrap';
 
 
 const LoginPage = () => {
@@ -35,13 +36,28 @@ const LoginPage = () => {
 
 
     return <>
-        <form onSubmit={loginUser} method='post'>
-            <h1>Login</h1>
+        {/* <form>
             <input type="text" name="email" id="email" placeholder='Votre email' onChange={handleChange} value={user.email} />
             <input type="password" name="password" id="password" placeholder='Votre mot de passe' onChange={handleChange} value={user.password} />
             <button type="submit">Valider</button>
         </form>
-        <a href='/signup'>Créer un compte</a>
+        <a href='/signup'>Créer un compte</a> */}
+        <Container fluid className='vh-100 d-flex align-items-center login-image-container'>
+            <Form onSubmit={loginUser} method='post' className='col-4 mx-auto d-flex flex-column mb-5'>
+                <h1 className='text-center'>Connexion</h1>
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Votre email</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="example@gmail.com" onChange={handleChange} value={user.email} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>Mot de passe</Form.Label>
+                    <Form.Control type="password" name="password" placeholder='Votre mot de passe' onChange={handleChange} value={user.password}/>
+                    <Button variant="link" className='ps-0' size="sm">Mot de passe oublié ?</Button>
+                </Form.Group>
+                <Button variant="primary" type="submit">Se connecter</Button>
+                <Button variant="link" onClick={() => {navigate('/signup')}} className='py-3'>Créer un compte</Button>
+            </Form>
+        </Container>
     </>
 }
 
