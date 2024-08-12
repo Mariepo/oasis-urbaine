7e4ebddc5accfe859aa36069dcfd20ec30a29bf5
Mariepo <marie.potte.62@gmail.com>
2024-08-12 16:38:10 +0200

    ajout des choix de livraison dans le panier

--- oasis-urbaine-api/Controllers/DeliveryMethodsController.js
+++ oasis-urbaine-api/Controllers/DeliveryMethodsController.js
@@ -2,6 +2,16 @@ const DeliveryMethodsService = require('../Services/DeliveryMethodsService')
 
 class DeliveryMethodsController {
 
+    async getAllDeliveryMethods(request, result) {
+        try {
+            const deliverymethods = await DeliveryMethodsService.getAllDeliveryMethods();
+            result.json(deliverymethods);
+        } catch (error) {
+            result.status(500);
+            result.json({error : "Une erreur est survenue lors de la récupération des modes de livraison"});
+        }
+    }
+
     async getDeliveryMethodById(request, result) {
         try {
             const deliverymethod = await DeliveryMethodsService.getDeliveryMethodById(request.params.id);
--- oasis-urbaine-api/Services/DeliveryMethodsService.js
+++ oasis-urbaine-api/Services/DeliveryMethodsService.js
@@ -2,6 +2,10 @@ const DeliveryMethods = require('../Models/DeliveryMethods');
 
 class DeliveryMethodsService {
 
+        async getAllDeliveryMethods(){
+            return await DeliveryMethods.findAll();
+        }
+
         async getDeliveryMethodById(id){
             return await DeliveryMethods.findByPk(id);
         }
