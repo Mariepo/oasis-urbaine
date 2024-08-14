88d1f12d62ff95e3230cb8a6d7cd65fa8aaa1f7c
Mariepo <marie.potte.62@gmail.com>
2024-08-14 08:55:25 +0200

    optimisation code /cart

--- oasis-urbaine-front/src/Pages/Cart.jsx
+++ oasis-urbaine-front/src/Pages/Cart.jsx
@@ -11,49 +11,42 @@ import { useNavigate } from "react-router-dom";
 function Cart() {
   const { cartItems, clearCart } = useContext(CartContext);
   const navigate = useNavigate();
-  // Méthodes de livraison
+  const id_user = UsersService.getUserId();
+
   const [deliveryMethods, setDeliveryMethods] = useState([]);
   const [selectedDeliveryMethodId, setSelectedDeliveryMethodId] = useState(1);
-  const selectedDeliveryMethod = deliveryMethods.find(deliveryMethod => deliveryMethod.id === selectedDeliveryMethodId);
-
-  // Méthodes de paiement
   const [paymentMethods, setPaymentMethods] = useState([]);
   const [selectedPaymentMethodId, setSelectedPaymentMethodId] = useState(1);
+  const [user, setUser] = useState([]);
 
-  // Total
+  const selectedDeliveryMethod = deliveryMethods.find(deliveryMethod => deliveryMethod.id === selectedDeliveryMethodId);
   const subtotal = cartItems.reduce((total, item) => total + (item.quantity * item.price), 0);
   const total = subtotal + (selectedDeliveryMethod ? Number(selectedDeliveryMethod.price) : 0);
 
-  // Services
-  const id_user = UsersService.getUserId();
-  const [user, setUser] = useState([]);
-  const fetchUser = async () => {
+  const fetchData = async () => {
       try {
-          const response = await UsersService.getUserById(id_user);
-          setUser(response.data);
+          const [deliveryResponse, paymentResponse] = await Promise.all([
+            DeliveryMethodsService.fetchDeliveryMethods(),
+            PaymentMethodsService.fetchPaymentMethods(),
+          ]);
+          setDeliveryMethods(deliveryResponse.data);
+          setPaymentMethods(paymentResponse.data);
+          if (id_user) {
+            const userResponse = await UsersService.getUserById(id_user);
+            setUser(userResponse.data);
+          }
       } catch (error) {
           console.log(error);
       }
   }
-  const fetchDeliveryMethods = async () => {
-    try {
-      const response = await DeliveryMethodsService.fetchDeliveryMethods();
-      setDeliveryMethods(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  const fetchPaymentMethods = async () => {
-    try {
-      const response = await PaymentMethodsService.fetchPaymentMethods();
-      setPaymentMethods(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  useEffect( () => {
+    fetchData();
+  }, []);
+
   const addOrder = async () => {
     if(id_user === null) {
       navigate('/login', {state: {from: '/cart'}});
+      return
     } else {
       try {
         const order = {
@@ -74,82 +67,73 @@ function Cart() {
     }
   }
 
-  // Handle change
-  const handleDeliveryChange = (event) => {
-    setSelectedDeliveryMethodId(Number(event.target.value));
-  }
-  const handlePaymentChange = (event) => {
-    setSelectedPaymentMethodId(Number(event.target.value));
+  const handleChange = (setter) => (event) => {
+    setter(Number(event.target.value));
   }
 
-  useEffect( () => {
-    fetchDeliveryMethods();
-    fetchPaymentMethods();
-    fetchUser();
-  }, []);
+  // const handleDeliveryChange = (event) => {
+  //   setSelectedDeliveryMethodId(Number(event.target.value));
+  // }
+  // const handlePaymentChange = (event) => {
+  //   setSelectedPaymentMethodId(Number(event.target.value));
+  // }
 
   const formatPrice = (price) => {
     const roundedPrice = Number(price).toFixed(2);
     return roundedPrice.endsWith(".00") ? Math.round(price) : roundedPrice;
   }
 
+  if (cartItems.length === 0){
+    return (
+      <Container>
+        <main className="col-md-8 mx-auto mt-5">
+          <section>
+            <h1>Votre Panier</h1>
+            <p>Votre panier est vide</p>
+          </section>
+        </main>
+      </Container>
+    )
+  }
+  
   return (
     <Container>
-      <main className="col-md-8 mx-auto">
-        <section>
-          <h1>Votre Panier</h1>
+      <main className="col-md-8 mx-auto my-5">
+          <section className="my-5">
+            <h1>Votre Panier</h1>
+          </section>
+        {cartItems.map(item => (
+          <CartItem key={item.id} item={item}></CartItem>
+        ))}
+        <section className="my-5">
+          <h2>Mode de livraison</h2>
+          {deliveryMethods.map((deliveryMethod) => (
+            <Form.Check type="radio" name="delivery" id={`delivery-${deliveryMethod.id}`} key={deliveryMethod.id} label={`${deliveryMethod.name} ${deliveryMethod.description} - ${deliveryMethod.price}€`} value={deliveryMethod.id} onChange={handleChange(setSelectedDeliveryMethodId)} checked={deliveryMethod.id === selectedDeliveryMethodId}
+            />
+          ))}
+          {id_user !== null && (
+            <Card className="mt-4">
+              <Card.Body>
+                <div>Adresse : {user.address}, {user.postal_code} {user.city} - {user.email} - {user.phone}</div>
+                <Button variant="link ps-0" onClick={()=>{navigate('/edit-address', {state: {from: '/cart'}})}}>Modifier</Button>
+              </Card.Body>
+            </Card> 
+          )} 
+        </section>          
+        <section className="my-5">
+          <h2>Paiement</h2>
+          {paymentMethods.map((paymentMethod) => (
+            <Form.Check type="radio" name="payment" id={`payment-${paymentMethod.id}`} key={paymentMethod.id} label={paymentMethod.name} value={paymentMethod.id} onChange={handleChange(setSelectedPaymentMethodId)} checked={paymentMethod.id === selectedPaymentMethodId}
+            />
+          ))}
         </section>
-        <section>
-          {cartItems.length === 0 ? (
-            <p>Votre panier est vide.</p>
-          ) : (
-            cartItems.map((item) => {
-              return (
-                <CartItem key={item.id} item={item}></CartItem>
-            )})
-          )}
+        <section className="text-end">
+          <hr />
+          <p>Sous-total : {subtotal}€</p>
+          <p>Livraison : {selectedDeliveryMethod ? `${formatPrice(selectedDeliveryMethod.price)}€` : 'Aucune'}</p>
+          <p>Total : {total}€</p>
+          <Button variant="primary" type="submit" onClick={addOrder}>Payer maintenant</Button>
         </section>
-        {cartItems.length > 0 && (
-          <>
-            <section>
-              <h2>Mode de livraison</h2>
-              {deliveryMethods.map((deliveryMethod) => (
-                  <Form.Check type="radio" name="delivery" id={`delivery-${deliveryMethod.id}`} key={deliveryMethod.id} label={`${deliveryMethod.name} ${deliveryMethod.description} - ${deliveryMethod.price}€`} value={deliveryMethod.id} onChange={handleDeliveryChange} checked={deliveryMethod.id === selectedDeliveryMethodId}
-                  />
-              ))}
-              <Card className="mt-4">
-                <Card.Body>
-                  <div>Adresse : {user.address}, {user.postal_code} {user.city} - {user.email} - {user.phone}</div>
-                  <Button variant="link ps-0" onClick={()=>{navigate('/edit-address', {state: {from: '/cart'}})}}>Modifier</Button>
-                </Card.Body>
-              </Card> 
-              <div className="pt-4">
-              </div>
-            </section>
-          </>
-        )}            
-        {cartItems.length > 0 && (
-          <>
-            <section>
-              <h2>Paiement</h2>
-              {paymentMethods.map((paymentMethod) => (
-                  <Form.Check type="radio" name="payment" id={`payment-${paymentMethod.id}`} key={paymentMethod.id} label={paymentMethod.name} value={paymentMethod.id} onChange={handlePaymentChange} checked={paymentMethod.id === selectedPaymentMethodId}
-                  />
-              ))}
-            </section>
-          </>
-        )}
-        {cartItems.length > 0 && (
-          <>
-            <section className="text-end">
-              <hr />
-              <p>Sous-total : {subtotal}€</p>
-              <p>Livraison : {selectedDeliveryMethod ? `${formatPrice(selectedDeliveryMethod.price)}€` : 'Aucune'}</p>
-              <p>Total : {total}€</p>
-              <Button variant="primary" type="submit" onClick={addOrder}>Payer maintenant</Button>
-            </section>
-          </>
-        )}
       </main>
     </Container>
   );
