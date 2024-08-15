abfc5fa71dd230a040204243333ba9116159d9b5
Mariepo <marie.potte.62@gmail.com>
2024-08-15 08:41:15 +0200

    ajout des routes pour la gestion des catégories dans le front

--- oasis-urbaine-front/src/Services/CategoriesService.js
+++ oasis-urbaine-front/src/Services/CategoriesService.js
@@ -5,6 +5,17 @@ class CategoriesService {
         return axios.get('http://127.0.0.1:3001/categories');
     }
 
+    static addCategorie(categorie) {
+        return axios.post('http://127.0.0.1:3001/categories', categorie)
+    }
+
+    static editCategorie(id, categorie) {
+        return axios.patch(`http://127.0.0.1:3001/categories${id}`, categorie)
+    }
+
+    static deleteCategorie(id){
+        return axios.delete(`http://127.0.0.1:3001/categories${id}`)
+    }
 }
 
 export default CategoriesService;
