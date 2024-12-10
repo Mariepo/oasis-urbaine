f55a25b927ae5713d7f95b018959317e5f0eac4f
Mariepo <marie.potte.62@gmail.com>
2024-12-10 12:18:03 +0100

    ajout Dockerfile

--- oasis-urbaine-front/src/Services/config.js
+++ oasis-urbaine-front/src/Services/config.js
@@ -1,4 +1,5 @@
-const SERVER_URL = 'http://192.168.1.15:3001';
-// const SERVER_URL = 'http://127.0.0.1:3001';
+const SERVER_URL = 'http://192.168.1.15:3334'; // Port docker
+// const SERVER_URL = 'http://192.168.1.15:3001'; // Port local
+// const SERVER_URL = 'http://127.0.0.1:3001'; // Port IP locale
 
 export default SERVER_URL;
