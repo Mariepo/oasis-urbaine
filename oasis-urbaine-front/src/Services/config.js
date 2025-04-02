c32fca995a644275a5f6472257eb9882dfdcd79c
Mariepo <marie.potte.62@gmail.com>
2025-04-02 10:43:25 +0200

    Revert "ajout Dockerfile"
    This reverts commit f55a25b927ae5713d7f95b018959317e5f0eac4f.

--- oasis-urbaine-front/src/Services/config.js
+++ oasis-urbaine-front/src/Services/config.js
@@ -1,5 +1,4 @@
-const SERVER_URL = 'http://192.168.1.15:3334'; // Port docker
-// const SERVER_URL = 'http://192.168.1.15:3001'; // Port local
-// const SERVER_URL = 'http://127.0.0.1:3001'; // Port IP locale
+const SERVER_URL = 'http://192.168.1.15:3001';
+// const SERVER_URL = 'http://127.0.0.1:3001';
 
 export default SERVER_URL;
