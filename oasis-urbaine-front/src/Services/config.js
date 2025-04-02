097b909dfee3f2fe02a829a01d634613c7bbf279
Mariepo <marie.potte.62@gmail.com>
2025-04-02 10:35:22 +0200

    modification config/SERVER_URL

--- oasis-urbaine-front/src/Services/config.js
+++ oasis-urbaine-front/src/Services/config.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'http://192.168.1.15:3001';
-// const SERVER_URL = 'http://127.0.0.1:3001';
+// const SERVER_URL = 'http://192.168.1.15:3001';
+const SERVER_URL = 'http://127.0.0.1:3001';
 
 export default SERVER_URL;
